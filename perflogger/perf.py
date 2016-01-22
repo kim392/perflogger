28f61432a421fd4d64a412432997246e04960f75
kim392 <kim392@illinois.edu>
2016-01-22 13:26:44 -0600

    Setters implemented for Perf and functions moved to Perf

--- perflogger/perf.py
+++ perflogger/perf.py
@@ -1,6 +1,73 @@
 import time
-
+import logging
+import platform
+import os
 class Perf(object):
 
-    def __init__(self):
-        self.timestamp = time.ctime()
+    def __init__(self, args, argv):
+        """
+        Return a Perf object whose command, project, and arguments are set by
+        user input
+        """
+        self.command = args.command
+        self.project = args.project
+        self.cmdargs = argv
+        self.setRunEnv()
+
+    def setPerfTime(self, perfTime):
+        """
+        Set the time taken to run the command
+        """
+        self.perfTime = perfTime
+
+    def setRunEnv(self):
+        """
+        Set the timestamp, Linux version, CPU information, and environment 
+        variables
+        """
+        # Get timestamp
+        self.ts = time.ctime() 
+        # Get Linux version and CPU info
+        proc = platform.processor()
+        machine = platform.machine()
+        node = platform.node()
+        arch = platform.architecture()
+        plat = platform.platform()
+        sys = platform.system()
+        release = platform.release()
+        ver = platform.version()
+        self.LinuxCPUInfo = (proc, machine, node, arch, plat, sys, release, ver)
+        # Get environment variables
+        env = ""
+        for envKey in os.environ.keys():
+            env += "%s=%s\n" % (envKey, os.environ[envKey])
+        self.env = env
+        logging.debug("Timestamp: %s; LinuxCPUInfo: %s; env: %s", 
+                      self.ts, self.LinuxCPUInfo, self.env)
+
+    def parseToJSON(self):
+        """
+        Parse the data into JSON format and insert into the Elasticsearch
+        Database.
+        """
+        print "Parsing to JSON..."
+        dataJSON = {}
+        return dataJSON
+        """
+        Format of JSON:
+        ---------------
+        json: {
+            runenv: {
+                nodes, cpu, uname
+            }
+            args:
+            command:
+            project:
+            user:
+            start: timestamp
+            end: timestamp
+            duration:
+            timings: {..., ..., ...}
+            success: true/false
+        }
+        """
